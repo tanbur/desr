9aa2ab7d512c653bff0f0616d8ef4b98355b099a
tanbur <richard.tanburn@hotmail.com>
2016-12-12 11:34:06 +0000

    Add normal Hermite multiplier and doctest

--- hermite_helper.py
+++ hermite_helper.py
@@ -56,7 +56,8 @@ def is_hnf_col(matrix_):
     return is_hnf_row(matrix_.T)
 
 def is_hnf_row(matrix_):
-    ''' Decide whether a matrix is in Hermite normal form, when acting on rows
+    ''' Decide whether a matrix is in Hermite normal form, when acting on rows.
+        This is according to the Havas Majewski Matthews definition.
 
     '''
     m, n = matrix_.shape
@@ -96,6 +97,8 @@ def hnf_row_lll(matrix_):
     while len(matrix_.shape) < 2:
         matrix_ = numpy.expand_dims(matrix_, axis=0)
     hnf, unimodular_matrix, rank = diophantine.lllhermite(matrix_, m1=1, n1=1)
+    assert numpy.abs(numpy.abs(numpy.linalg.det(unimodular_matrix)) - 1) < 1e-10
+    assert is_hnf_row(hnf)
     return hnf, unimodular_matrix
 
 def hnf_col_lll(matrix_):
@@ -104,6 +107,89 @@ def hnf_col_lll(matrix_):
         Output: small unimodular matrix B and HNF(A), such that BA=HNF(A)+
         The Havas, Majewski, Matthews LLL method is used
         We usually take alpha=m1/n1, with (m1,n1)=(1,1) to get best results
+
+        >>> A = numpy.array([[8, 2, 15, 9, 11],
+        ...                  [6, 0, 6, 2, 3]])
+        >>> h, v = hnf_col(A)
+        >>> print h
+        [[1 0 0 0 0]
+         [0 1 0 0 0]]
+        >>> print v
+        [[-1  0  0 -1 -1]
+         [-3 -1  6 -1  0]
+         [ 1  0  1  1  2]
+         [ 0 -1 -3 -3  0]
+         [ 0  1  0  2 -2]]
+         >>> numpy.all(numpy.dot(A, v) == h)
+         True
     '''
     hnf, unimod = hnf_row_lll(matrix_.T)
     return hnf.T, unimod.T
+
+
+## Normal Hermite multiplier from Hubert-Labahn
+def is_normal_hermite_multiplier(hermite_multiplier, matrix_):
+    ''' Determine whether hermite_multiplier is the normal Hermite multiplier of matrix.
+        This is the COLUMN version.
+    '''
+    if numpy.linalg.matrix_rank(matrix_) != matrix_.shape[0]:
+        raise ValueError('Matrix must have full row rank')
+
+    r, n = matrix_.shape
+    if hermite_multiplier.shape != (n, n):
+        raise ValueError("Matrix dimensions don't match")
+
+    # Check matrix . hermite_multiplier = [H 0]
+    prod = numpy.dot(matrix_, hermite_multiplier)
+    hermite_form, residue = prod[:, :r], prod[:, r:]
+    # Condition a)
+    if not numpy.all(residue == 0):
+        return False
+    if not is_hnf_col(hermite_form):
+        return False
+    # Condition b)
+    herm_mul_i, herm_mul_n = hermite_multiplier[:, :r], hermite_multiplier[:, r:]
+    if not is_hnf_col(herm_mul_n):
+        return False
+    # Condition c)
+    for i in xrange(r):
+        pivot_val = numpy.max(herm_mul_n[i])
+        if numpy.any(numpy.abs(herm_mul_i[i]) >= pivot_val):
+            return False
+
+    return True
+
+def normal_hnf_col(matrix_):
+    ''' Return the hnf and the unique normal Hermite multiplier
+
+        >>> A = numpy.array([[8, 2, 15, 9, 11],
+        ...                  [6, 0, 6, 2, 3]])
+        >>> h, v = normal_hnf_col(A)
+        >>> print h
+        [[1 0 0 0 0]
+         [0 1 0 0 0]]
+        >>> print v
+        [[  0   0   1   0   0]
+         [  0   0   0   1   0]
+         [  2   1   3   1   5]
+         [  9   2  21   3  21]
+         [-10  -3 -22  -4 -24]]
+         >>> numpy.all(numpy.dot(A, v) == h)
+         True
+    '''
+    r, n = matrix_.shape
+    out, _ = hnf_col_lll(numpy.vstack((matrix_, numpy.eye(n))))
+    h, v = out[:r], out[r:]
+    from numpy.testing import assert_array_equal
+    assert_array_equal(numpy.dot(matrix_, v), h)
+    print 'is normal hh:', is_normal_hermite_multiplier(hermite_multiplier=v, matrix_=matrix_)
+    return h, v
+
+## Set defaults
+hnf_row = hnf_row_lll
+hnf_col = hnf_col_lll
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
